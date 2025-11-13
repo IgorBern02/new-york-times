df2a3d708bb4ea74c003840fb83bfc0646cd590f
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2025-11-13 19:25:40 -0300

    feat: footer adicionado

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,11 +2,11 @@ export const Footer = () => {
   return (
     <div className="w-full h-40 p-2 flex flex-col justify-center items-center bg-gray-200 mt-10">
       <h1 className="font-bebasneue text-base">Timesfeed newspaper</h1>
-      <div className="flex flex-col justify-center items-center text-xs gap-2 mt-4 ">
-        <ul>
-          <li>About us</li>
-          <li>Privacy Policy</li>
-          <li>Terms of use</li>
+      <div className="text-xs mt-4 ">
+        <ul className="flex flex-col gap-2">
+          <li className="cursor-pointer hover:text-red-500">About us</li>
+          <li className="cursor-pointer hover:text-red-500">Privacy Policy</li>
+          <li className="cursor-pointer hover:text-red-500">Terms of use</li>
         </ul>
       </div>
     </div>
